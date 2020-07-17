c7d6f33d8a4cfc861131a9a388de16ec2778d033
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-07-17 17:48:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,7 +25,12 @@ class Dog
     DB[:conn].execute(sql)
   end
   
-  def self. new_from_db
+    def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
+  end  
   def save
     if self.id
       self.update
