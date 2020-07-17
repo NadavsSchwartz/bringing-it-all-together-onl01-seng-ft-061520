1ef65131a4a89fe0ae5f28fb3159c7925dc29037
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-07-17 17:48:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,6 +32,12 @@ class Dog
     self.new(id, name, breed)
   end  
   
+    def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Dog.new(result[0], result[1], result[2])
+  end
+  
   def save
     if self.id
       self.update
