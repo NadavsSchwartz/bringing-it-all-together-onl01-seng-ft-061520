956bbe44c2aea533dc66bf592bc0761d31defe28
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-07-17 17:33:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,17 +2,10 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
   
-  def initialize(db)
-    @name = db["name"]
-    @breed = db["breed"]
-    @id = db["id"]
+  def initialize(id:, name:, breed:)
+    @name = name
+    @breed = dbbreed
+    @id = id
   end
   
-  def self.attributes(row)
-    new_dog = Dog.new
-    new_dog.id = row[0]
-    new_dog.name =  row[1]
-    new_dog.breee = row[2]
-    new_dog
-  end
 end
