cf9c2a8c6e6330fde646e23253b62dbb17cf9097
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-07-17 18:09:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -38,6 +38,8 @@ class Dog
     Dog.new(id: result[0],name: result[1],breed: result[2])
   end
   
+  def self.find_or_create_by
+  
   def self.create(name:, breed:)
     dog = Dog.new(name: name, breed: breed)
     dog.save
